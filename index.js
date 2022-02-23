05139e331c9c804770b07c278c88449c97fa7af7
ifiyenia-ea <89512351+ifiyenia-ea@users.noreply.github.com>
2022-02-23 16:48:40 +0100

    finishing for the day

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-ReactDOM.render(<h2>Let's do this!</h2>, document.getElementById("root"));
-
-ReactDOM.render(
-  <ul>
-    <li> one</li>
-    <li> one</li>
-    <li> one</li>
-    <li> one</li>
-    <li> one</li>
-  </ul>,
-  document.getElementById("list")
-);
-
-function MainContent() {
-  return <h1>I'm learning react again!</h1>;
-}
-
-function AnotherOne() {
-  return <h3>...which apparently is well needed...</h3>;
-}
-
-ReactDOM.render(
+const navbar = (
   <div>
-    <MainContent />
-    <AnotherOne />
-  </div>,
-  document.getElementById("content")
+    <nav>
+      <h1>athenas uggla</h1>
+      <ul>
+        <li>om</li>
+        <li>tjänster</li>
+        <li>kontakt</li>
+      </ul>
+    </nav>
+  </div>
 );
+
+const content = (
+  <div>
+    <h2>din digitala assistent</h2>
+    <p>Vad behöver du hjälp med?</p>
+  </div>
+);
+
+ReactDOM.render(navbar, document.getElementById("nav"));
+ReactDOM.render(content, document.getElementById("content"));
